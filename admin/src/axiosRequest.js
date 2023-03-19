0a0b3b02cde8ed75c1e93fda1bae8ce41e6e3c8f
Nuellity <juxnuel@gmail.com>
2023-03-18 17:25:29 -0700

    edit typo error in axiosRequest

--- admin/src/axiosRequest.js
+++ admin/src/axiosRequest.js
@@ -15,5 +15,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
     baseURL: BASE_URL,
-    header: {token: `Bearer ${token}`}
+    headers: {token: `Bearer ${token}`}
 })
