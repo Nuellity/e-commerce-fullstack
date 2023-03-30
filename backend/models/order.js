1454175fab09f79bd42e63b402f75f4aaa4900c3
Nuellity <juxnuel@gmail.com>
2023-03-29 19:13:46 -0700

    add stripe payment intent to order api

--- backend/models/order.js
+++ backend/models/order.js
@@ -16,6 +16,7 @@ const orderSchema = new mongoose.Schema(
     amount: { type: Number, required: true },
     address: { type: Object, required: true },
     status: { type: String, default: "pending" },
+    paymentIntent: { type: String, required: true },
   },
   { timestamps: true }
 );
