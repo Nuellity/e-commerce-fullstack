a36e18962cc52c6dfaa765cc7c0a40d22dba3fbb
Nuellity <juxnuel@gmail.com>
2023-03-31 05:38:30 -0700

    add daily orders update

--- admin/src/components/StatBox.jsx
+++ admin/src/components/StatBox.jsx
@@ -3,40 +3,50 @@ import { Box, Typography, useTheme } from "@mui/material";
 import { tokens } from "../theme";
 import ProgressCircle from "./ProgressCircle";
 
-function StatBox({ title, subTitle, icon, progress, increase }) {
+function StatBox({
+  title,
+  subTitle,
+  icon,
+  progress,
+  increase,
+  isProgress,
+  isIncrease,
+}) {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
   return (
-    <Box component="div" sx={{ padding: "5px", width: "100%", margin: "15px 20px"}}>
+    <Box
+      component="div"
+      sx={{ padding: "5px", width: "100%", margin: "15px 20px" }}
+    >
       <Box display="flex" justifyContent="space-between">
         <Box>
           {icon}
           <Typography
             variant="h4"
             fontWeight="bold"
-            sx={{ color: colors.grey[100] }}
+            sx={{ color: colors.grey[100], margin: "5px" }}
           >
             {title}
           </Typography>
         </Box>
-        <Box>
+        <Box sx={{ display: isProgress && "none" }}>
           <ProgressCircle progress={progress} />
         </Box>
-        </Box>
-        <Box display="flex" justifyContent="space-between" mt="2px">
-          <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
-            {subTitle}
-          </Typography>
-          <Typography
-            variant="h5"
-            fontStyle="italic"
-            sx={{ color: colors.greenAccent[600] }}
-          >
-            {increase}
-          </Typography>
-        </Box>
-      
+      </Box>
+      <Box display="flex" justifyContent="space-between" mt="2px">
+        <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
+          {subTitle}
+        </Typography>
+        <Typography
+          variant="h5"
+          fontStyle="italic"
+          sx={{ color: colors.greenAccent[600], display: isIncrease && "none" }}
+        >
+          {increase}
+        </Typography>
+      </Box>
     </Box>
   );
 }
