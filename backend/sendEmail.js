0667aad9a847a62a123b78fcb498fdaf7125459c
Nuellity <juxnuel@gmail.com>
2023-06-26 02:39:29 +0100

    edit sendEmail

--- backend/sendEmail.js
+++ backend/sendEmail.js
@@ -5,7 +5,7 @@ const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: 587,
-      secure: false,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
