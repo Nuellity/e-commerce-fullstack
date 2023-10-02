2e824f0dd341670a841d08e5acdbf59590c64fb5
Nuellity <juxnuel@gmail.com>
2023-10-02 11:19:21 +0100

    make marquee responsive

--- frontend/src/pages/Homepage/HomeBrand.jsx
+++ frontend/src/pages/Homepage/HomeBrand.jsx
@@ -4,37 +4,13 @@ import Marquee from "react-fast-marquee";
 function HomeBrand() {
   return (
     <Marquee speed={100} gradient={false}>
-      <div className="row m-0 py-5">
-        <img
-          className="img-fluid col-lg-2 col-md-4 col-6"
-          src="images/img/brand/1.png"
-          alt=""
-        />
-        <img
-          className="img-fluid col-lg-2 col-md-4 col-6"
-          src="images/img/brand/2.png"
-          alt=""
-        />
-        <img
-          className="img-fluid col-lg-2 col-md-4 col-6"
-          src="images/img/brand/3.png"
-          alt=""
-        />
-        <img
-          className="img-fluid col-lg-2 col-md-4 col-6"
-          src="images/img/brand/4.png"
-          alt=""
-        />
-        <img
-          className="img-fluid col-lg-2 col-md-4 col-6"
-          src="images/img/brand/5.png"
-          alt=""
-        />
-        <img
-          className="img-fluid col-lg-2 col-md-4 col-6"
-          src="images/img/brand/6.png"
-          alt=""
-        />
+      <div className="d-flex gap-3 justify-content-around py-5">
+        <img src="images/img/brand/1.png" alt="" />
+        <img src="images/img/brand/2.png" alt="" />
+        <img src="images/img/brand/3.png" alt="" />
+        <img src="images/img/brand/4.png" alt="" />
+        <img src="images/img/brand/5.png" alt="" />
+        <img src="images/img/brand/6.png" alt="" />
       </div>
     </Marquee>
   );
